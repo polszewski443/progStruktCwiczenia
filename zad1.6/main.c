1f412553020b64134477a5f94192e86e807f85cd
polszewski443 <70515373+polszewski443@users.noreply.github.com>
2021-03-05 18:43:27 +0100

    Add files via upload
    dodanie zadania domowego

---
+++ zad1.6/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<string.h>
+
+int main()
+{
+    int i;
+    int letters = 97;
+    char tab[25];
+    for (i = 0; i<26; i++)
+    {
+        tab[i] = (char)letters;
+        letters++;
+    }
+    for (i = 0; i<26; i++)
+        printf("%c ", tab[i]);
+    return 0;
+}

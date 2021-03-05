ff79948add79f259d4f6b190dd28a91dc17d809c
polszewski443 <70515373+polszewski443@users.noreply.github.com>
2021-03-05 16:54:12 +0100

    Add files via upload
    dodanie zadania domowego

---
+++ zad1.6/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int miau = 2;
+    miau += 5;
+    miau *= 10;
+    miau -= 6;
+    miau /= 9;
+    miau %= 3;
+    printf("Miau wynosi: %d", miau);
+    return 0;
+}

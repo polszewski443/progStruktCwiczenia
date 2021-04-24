d19dc665292381380af2c00b69b3341d2a301974
polszewski443 <70515373+polszewski443@users.noreply.github.com>
2021-04-24 18:24:53 +0200

    Update main.c
    poprawilem wczesniejszy kod, teraz dziala poprawnie

--- zad5.2.5/main.c
+++ zad5.2.5/main.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 void przepisz(const char *, char *);
 int main()
 {
-    const char n1[30] = "NapisŸuódœŸ¹œ";
-    char n2[30];
-    przepisz(&n1, &n2);
-    for (int i = 0; i<(sizeof(char)/sizeof(n2)); i++)
-        printf("%c ", n2[i]);
+    const char n1[] = "vghslvnasfplf";
+    char n2[0];
+    przepisz(n1, n2);
+    printf("%s ", n2);
     return 0;
 }
 void przepisz(const char *n1, char *n2)
 {
-   strcpy(*n2,*n1);
+   strcpy(n2,n1);
 }
-//kod niestety nie dziala, poprawie go gdy znajde rozwiazanie problemu
